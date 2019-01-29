7d3fe87ec4f6780a0a43c436cd49784bf8094539
Luke <lukeknoot@gmail.com>
2019-01-29 20:19:43 +1100

    ex1-3

--- src/Set1.hs
+++ src/Set1.hs
@@ -22,4 +22,27 @@ randString3 = [one, two, three]
   (one  , s1) = randLetter $ mkSeed 1
   (two  , s2) = randLetter s1
   (three, _ ) = randLetter s2
-  
+
+randEven :: Gen Integer 
+randEven = generalA (* 2)
+
+randOdd :: Gen Integer 
+randOdd = generalA ((+ 1) . (* 2))
+
+randTen :: Gen Integer 
+randTen = generalA (* 10)
+
+generalA :: (Integer -> Integer) -> Gen Integer
+generalA l s = (l i, s') where (i, s') = rand s
+
+randEven2 :: Gen Integer 
+randEven2 = generalA2 (* 2) rand
+
+randOdd2 :: Gen Integer 
+randOdd2 = generalA2 ((+ 1) . (* 2)) rand
+
+randTen2 :: Gen Integer 
+randTen2 = generalA2 (* 10) rand
+
+generalA2 :: (a -> b) -> Gen a -> Gen b 
+generalA2 l r s = (l i, s') where (i, s') = r s
--- test/Spec.hs
+++ test/Spec.hs
@@ -15,3 +15,19 @@ main = hspec $ do
   describe "randString3" $ 
     it "should sha256 to 9d475eb78d3e38085220ed6ebde9d8f7d26540bb1c8f9382479c3acd4c8c94a3" $ 
       show (hashWith SHA256 $ pack randString3) `shouldBe` "9d475eb78d3e38085220ed6ebde9d8f7d26540bb1c8f9382479c3acd4c8c94a3"
+  describe "randEven randOdd randTen generalA" $
+    it "should multiply to 189908109902700 given a seed of 1" $
+        let 
+          (i, s1) = randEven $ mkSeed 1
+          (i2, s2) = randOdd $ mkSeed 1
+          (i3, s3) = randTen $ mkSeed 1
+        in 
+          product [i, i2, i3] `shouldBe` 189908109902700
+  describe "randEven2 randOdd2 randTen2 generalA2" $
+    it "should multiply to 189908109902700 given a seed of 1" $
+        let 
+          (i, s1) = randEven2 $ mkSeed 1
+          (i2, s2) = randOdd2 $ mkSeed 1
+          (i3, s3) = randTen2 $ mkSeed 1
+        in 
+          product [i, i2, i3] `shouldBe` 189908109902700
