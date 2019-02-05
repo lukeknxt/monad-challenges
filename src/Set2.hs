c36207f4c5824cd3123dec2ac80a3993d0987f75
Luke <lukeknoot@gmail.com>
2019-02-05 19:24:07 +1100

    ex2-1

--- src/Set2.hs
+++ src/Set2.hs
@@ -5,3 +5,9 @@ module Set2 where
 
 import           MCPrelude
 
+data Maybe a = Nothing | Just a
+
+instance Show a => Show (Maybe a) where
+  show m = case m of
+    Just a -> "Just " ++ show a 
+    Nothing -> "Nothing"
