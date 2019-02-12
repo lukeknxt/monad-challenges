680df04d3c78d571ba904530c648704cbb3ef3a9
Luke <lukeknoot@gmail.com>
2019-02-12 19:03:32 +1100

    ex2-2

--- src/Set2.hs
+++ src/Set2.hs
@@ -5,9 +5,37 @@ module Set2 where
 
 import           MCPrelude
 
-data Maybe a = Nothing | Just a
+data Maybe a = Nothing | Just a deriving Eq
 
 instance Show a => Show (Maybe a) where
   show m = case m of
-    Just a -> "Just " ++ show a 
+    Just a  -> "Just " ++ show a
     Nothing -> "Nothing"
+
+headMay :: [a] -> Maybe a
+headMay []       = Nothing
+headMay (x : xs) = Just x
+
+tailMay :: [a] -> Maybe [a]
+tailMay []       = Nothing
+tailMay (x : []) = Just []
+tailMay (x : xs) = Just xs
+
+lookupMay :: Eq a => a -> [(a, b)] -> Maybe b
+lookupMay a []  = Nothing
+lookupMay a [x] = if (fst x) == a then Just (snd x) else Nothing
+lookupMay a l   = lookupMay a (filter (\(one, two) -> one == a) l)
+
+divMay :: (Eq a, Fractional a) => a -> a -> Maybe a
+divMay x y | y == 0    = Nothing
+           | otherwise = Just $ x / y
+
+maximumMay :: Ord a => [a] -> Maybe a
+maximumMay []           = Nothing
+maximumMay [x         ] = Just x
+maximumMay (x : y : xs) = maximumMay ((if x >= y then x else y) : xs)
+
+minimumMay :: Ord a => [a] -> Maybe a
+minimumMay []           = Nothing
+minimumMay [x         ] = Just x
+minimumMay (x : y : xs) = minimumMay ((if x <= y then x else y) : xs)
--- test/Set2Spec.hs
+++ test/Set2Spec.hs
@@ -2,12 +2,49 @@ module Set2Spec where
 
 import           Test.Hspec
 import           MCPrelude
+import Set2
 
 spec :: Spec
 spec = do
-  describe "test" $
-    it "test" $ 
-      1 == 1
-  describe "test" $
-    it "test" $
-      True
+  describe "headMay" $ do
+    it "should handle empty list" $ 
+      shouldBe (headMay [] :: Maybe Integer) $ Nothing
+    it "should handle single element list" $ 
+      shouldBe (headMay [1]) $ Just 1
+    it "should handle multi-element list" $ 
+      shouldBe (headMay [1, 2]) (Just 1)
+  describe "tailMay" $ do
+    it "should handle empty list" $ 
+      shouldBe (tailMay [] :: Maybe [Integer]) $ Nothing
+    it "should handle single element list" $ 
+      shouldBe (tailMay [1]) $ Just [] 
+    it "should handle multi-element list" $ 
+      shouldBe (tailMay [1, 2]) (Just [2])
+    it "should handle multi-element tail" $ 
+      shouldBe (tailMay [1, 2, 3]) (Just [2, 3])
+  describe "lookupMay" $ do
+    it "should handle empty list" $ 
+      shouldBe (lookupMay 1 [] :: Maybe Integer) $ Nothing
+    it "should handle single element list" $ 
+      shouldBe (lookupMay 1 [(1, 2)]) $ Just 2
+    it "should handle multi-element list" $ 
+      shouldBe (lookupMay 3 [(1, 2), (3, 4)]) $ Just 4
+  describe "divMay" $ do
+    it "should handle division by zero" $ 
+      shouldBe (divMay 2 0) $ Nothing
+    it "should handle division by non-zero" $ 
+      shouldBe (divMay 2 1) $ Just 2
+  describe "maximumMay" $ do
+    it "should handle empty list" $ 
+      shouldBe (maximumMay [] :: Maybe Integer) $ Nothing
+    it "should handle single element list" $ 
+      shouldBe (maximumMay [1]) $ Just 1
+    it "should handle multi-element list" $ 
+      shouldBe (maximumMay [1, 2]) $ Just 2
+  describe "minimumMay" $ do
+    it "should handle empty list" $ 
+      shouldBe (minimumMay [] :: Maybe Integer) $ Nothing
+    it "should handle single element list" $ 
+      shouldBe (minimumMay [1]) $ Just 1
+    it "should handle multi-element list" $ 
+      shouldBe (minimumMay [1, 2]) $ Just 1
